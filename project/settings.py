8f82f14c317a66f9ff84cd905d4203ec5b3bfbcc
이름이다 <raeun19@naver.com>
2019-05-27 19:44:45 +0900

    first commit

--- project/settings.py
+++ project/settings.py
@@ -45,7 +45,7 @@ MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
+
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
